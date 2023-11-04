f0ef0a95a95688e89264156418bb468d1647e242
LunatikTanuki <luna.hammesfahr@gmail.com>
2023-11-04 16:23:05 +0100

    text-to-spech function

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,60 +1,42 @@
-import os
 import torch
-from transformers import pipeline, AutoProcessor, AutoModel, BarkModel
-from bark import SAMPLE_RATE, generate_audio, preload_models
-from IPython.display import Audio
-from scipy.io.wavfile import write
+import simpleaudio
+from transformers import AutoProcessor, BarkModel
 
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+# TODO: Fix warning: "torch.nn.utils.weight_norm is deprecated in favor of torch.nn.utils.parametrizations.weight_norm."
+# TODO: The attention mask and the pad token id were not set. As a consequence, you may observe unexpected behavior.
+#  Please pass your input's `attention_mask` to obtain reliable results.
+# Setting `pad_token_id` to `eos_token_id`:10000 for open-end generation.
 
-# synthesiser = pipeline("text-to-speech", model="suno/bark-small")
-# speech = synthesiser("Wie geht es dir?", forward_params={"do_sample": True})
-# Audio(speech, rate=SAMPLE_RATE)  # TypeError
-# exit(0)
 
-# # Optimizing:
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# # load in fp16
-# model = BarkModel.from_pretrained("suno/bark-small", torch_dtype=torch.float16).to(device)
-# # convert to bettertransformer
-# model = model.to_bettertransformer()
-# # model = BetterTransformer.transform(model, keep_original_model=False)
-# # enable CPU offload
-# model.enable_cpu_offload()
+def text_to_audio(input_text):
+    """
+    Converts text into audio and plays it.
+    """
+    # TODO: Setup Cuda
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-processor = AutoProcessor.from_pretrained("suno/bark-small")
-model = AutoModel.from_pretrained("suno/bark-small")  # .to(device)  # Or BarkModel?
+    processor = AutoProcessor.from_pretrained("suno/bark-small")
+    model = BarkModel.from_pretrained("suno/bark-small", ).to(device)  # Add torch_dtype=torch.float16 after model to load in fp16
+    # Optimize the speed by converting the model into a BetterTransformer
+    model = model.to_bettertransformer()  # model = BetterTransformer.transform(model, keep_original_model=False)
+    # model.enable_cpu_offload(), when using a CUDA device
 
-voice_preset = "v2/en_speaker_6"
-inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)  # , return_tensors="pt") or .to(device)
+    voice_preset = "v2/de_speaker_3"  # German speakers: v2/de_speaker_0 to 9 (speakers 3 and 8 are female)
+    inputs = processor(input_text, voice_preset=voice_preset).to(device)  # , return_tensors="pt")
 
-audio_array = model.generate(**inputs)  # , do_sample=True)
-audio_array = audio_array.cpu().numpy().squeeze()
+    audio_array = model.generate(**inputs)  # , do_sample=True)
+    audio_array = audio_array.cpu().numpy().squeeze()
+    sampling_rate = model.generation_config.sample_rate
 
-sampling_rate = model.generation_config.sample_rate
-Audio(audio_array, rate=sampling_rate)
-exit(0)
+    # Play audio. To save audio to disk: write("bark_generation.wav", sampling_rate, audio_array)
+    num_channels = 1
+    bytes_per_sample = 4  # float32
+    play_obj = simpleaudio.play_buffer(audio_array, num_channels, bytes_per_sample, sampling_rate)
 
-# Save audio to disk (working)
-# write("bark_generation.wav", sampling_rate, audio_array)
+    # Wait for playback to finish before exiting
+    play_obj.wait_done()
 
-text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
-"""
-audio_array = generate_audio(text_prompt)
-Audio(audio_array, rate=SAMPLE_RATE)
 
-# Running locally with bark:
-# download and load all models
-preload_models()
-
-# generate audio from text
-text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
-"""
-speech_array = generate_audio(text_prompt)
-
-# play text in notebook
-Audio(speech_array, rate=SAMPLE_RATE)
+if __name__ == "__main__":
+    text_input = """Hallo, wie geht es dir?"""
+    text_to_audio(text_input)
