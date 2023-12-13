9da12fdbd1af1d1bbae48bb39e4d9325a5053fe1
LunatikTanuki <luna.hammesfahr@gmail.com>
2023-12-13 10:06:16 +0100

    Update questions for chapter 7.3 and comments in script

--- dataprocessing/process_vectorstore.py
+++ dataprocessing/process_vectorstore.py
@@ -1,6 +1,8 @@
-import os
-import re
+"""
+Script for creating a vector store from text files or pdf data.
+"""
 
+import os
 import torch
 
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
@@ -19,9 +21,7 @@ DB_FAISS_PATH = os.path.join(directory, 'data/vectorStore')
 
 def get_doc_from_pdf():
     """
-    Read pdf documents.
-
-    :return documents: Read pdf documents.
+    Reads pdf files and return a list of documents.
     """
     loader = DirectoryLoader(PDF_PATH, glob='*.pdf', loader_cls=PyPDFLoader)
     documents = loader.load()
@@ -30,41 +30,16 @@ def get_doc_from_pdf():
 
 def get_doc_from_txt():
     """
-    Read textfiles.
-
-    :return documents: Read textfiles.
+    Reads textfiles and return a list of documents.
     """
     loader = DirectoryLoader(TXT_PATH, glob='*.txt', loader_cls=TextLoader)
     documents = loader.load()
     return documents
 
 
-def create_vector_db(documents):
-    """
-    Create vector database from text documents.
-
-    :param documents: Text documents to create the vector store from.
-    """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
-    split_text = text_splitter.split_documents(documents)
-    # docs = [langchain.docstore.document.Document(page_content=t) for t in split_text[:len(split_text)]]
-
-    embeddings = HuggingFaceEmbeddings(model_name='paraphrase-multilingual-MiniLM-L12-v2',
-                                       # Or: model_name='LLukas22/all-MiniLM-L12-v2-embedding-all',
-                                       model_kwargs={'device': device})
-
-    db = FAISS.from_documents(split_text, embeddings)
-    db.save_local(DB_FAISS_PATH)
-    print(f"Database saved to {DB_FAISS_PATH}")
-
-
 def process_doc(documents):
     """
-    Remove headlines and questions from page content.
-
-    :param documents: Text documents to create the vector store from.
+    Removes headlines as well as questions and answers from the page contents of each document.
     """
     for document in documents:
         processed_text = " ".join(document.page_content.split("\n", 2)[2:]).split('\n')
@@ -75,8 +50,25 @@ def process_doc(documents):
     return documents
 
 
-if __name__ == '__main__':
-    # texts = get_doc_from_pdf()
-    texts = get_doc_from_txt()
-    process_doc(texts)
-    create_vector_db(texts)
+def create_vector_db(documents):
+    """
+    Creates a FAISS vector database from text documents. First splits the text recursively and then creates embeddings.
+    """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+    split_text = text_splitter.split_documents(documents)
+    # docs = [langchain.docstore.document.Document(page_content=t) for t in split_text[:len(split_text)]]
+
+    embeddings = HuggingFaceEmbeddings(model_name='paraphrase-multilingual-MiniLM-L12-v2',
+                                       model_kwargs={'device': device})
+
+    db = FAISS.from_documents(split_text, embeddings)
+    db.save_local(DB_FAISS_PATH)
+    print(f"Database saved to {DB_FAISS_PATH}")
+
+
+# texts = get_doc_from_pdf()
+texts = get_doc_from_txt()
+process_doc(texts)
+create_vector_db(texts)
