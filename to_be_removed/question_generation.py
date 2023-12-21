2635a8f7336db6829b73c1cddba013f8ee74e02c
LunatikTanuki <luna.hammesfahr@gmail.com>
2023-12-21 18:49:05 +0100

    Tests

--- to_be_removed/question_generation.py
+++ to_be_removed/question_generation.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+
+from langchain.chains import RetrievalQA
 from lmqg import TransformersQG
 
 # initialize model
@@ -14,3 +16,31 @@ context = "Wie bereits erwähnt sind Objekte im Speicher abgelegte Daten. Dabei 
 question_answer = model.generate_qa(context)
 # the output is a list of tuple (question, answer)
 pprint(question_answer)
+
+
+def get_question_from_retriever(self, query, k=4):
+    """
+    TODO: remove this function?
+    Retrieve context for a given topic from a FAISS database.
+
+    :param query:
+    :param k:
+
+    :return result: Answer to the question.
+    :return source_documents: Source documents from the database.
+    """
+    # Set up as generic retriever
+    retriever = self.db.as_retriever(search_type="similarity", search_kwargs={'k': k})
+
+    # Create the chain for question answering
+    answer_chain = RetrievalQA.from_chain_type(
+        llm=self.llm,
+        chain_type="stuff",  # stuff, map_reduce or map_rerank with k=10
+        retriever=retriever,  # context_docs = db.similarity_search(query, k=4)
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': self.prompt}
+    )
+
+    result = answer_chain({'query': query})
+    result = result["result"]  # Sources at: result["source_documents"]
+    return result
