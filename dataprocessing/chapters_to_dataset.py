e31bfe022c4d762f5f5dc8d4e4646e6e7b15f49b
LunatikTanuki <luna.hammesfahr@gmail.com>
2024-01-31 20:42:45 +0100

    Dataset from processed chapters

--- dataprocessing/chapters_to_dataset.py
+++ dataprocessing/chapters_to_dataset.py
@@ -7,26 +7,28 @@ import os
 
 directory = os.path.dirname(os.path.dirname(__file__))
 INPUT_PATH = os.path.join(directory, 'data/chapters_processed')
-OUTPUT_FILE = os.path.join(directory, 'data/processed/chapters.csv')
+OUTPUT_FILE = os.path.join(directory, 'data/dataset/train.csv')
+
 
 count = 0
 # Get text from txt files
 for file in glob.glob(INPUT_PATH + '/*.txt'):
     with open(file, 'r') as txt_file:
         lines = txt_file.readlines()
-        # Remove the title from the lines
+        # Remove the title and empty lines
         lines = lines[lines.index('\n') + 1:]
+        lines = [line for line in lines if line.strip()]
 
-        # Get the next paragraph and remove it from the lines
-        while lines and '\n' in lines:
-            paragraph = ''.join(lines[:lines.index('\n')]).rstrip()
-            lines = lines[lines.index('\n') + 1:]
+        while lines:
+            # Get the next paragraph and remove it from the lines
+            paragraph = ""
+            while lines and not lines[0].startswith("Frage:") and not lines[0].startswith("Antwort:"):
+                paragraph += lines.pop(0).rstrip()
 
             # Get the next question and its answer and remove them from the lines
-            while lines[0][:5] == "Frage":
-                question = lines[0].split(':', 1)[1].strip()  # TODO: Catch errors?
-                answer = lines[1].split(':', 1)[1].strip()
-                lines = lines[3:]
+            while lines and lines[0].startswith("Frage:"):
+                question = lines.pop(0).split(':', 1)[1].strip()
+                answer = lines.pop(0).split(':', 1)[1].strip()
 
                 # Create a row with the paragraph, the question and the answer and write it to the CSV file
                 text_context = [paragraph, question, answer]
