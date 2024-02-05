8cd0551569958172c0ef6fbe62c7feb81b4099f6
LunatikTanuki <luna.hammesfahr@gmail.com>
2024-02-05 23:04:32 +0100

    Evaluate feedback prompt tests

---
+++ dataprocessing/evaluate_feedback_tests.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+directory = os.path.dirname(os.path.dirname(__file__))
+INPUT_FILE = os.path.join(directory, 'testing/prompt_feedback_01.txt')
+words = [" du ", "inhaltlich", "relevant", "vollständig", "präzis", "Hilfe", "Leistung", "Thema"]
+occurrence = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# Get text from txt files
+with open(INPUT_FILE, 'r') as txt_file:
+    lines = txt_file.readlines()
+    # Remove the title and empty lines
+    spaces = '                      '
+    for i, line in reversed(list(enumerate(lines))):
+        if line.startswith('                      '):
+            lines[i-1] += line
+            lines.pop(i)
+    for line in lines:
+        if line.startswith("Generiertes Feedback:"):
+            for k in range(len(words)):
+                if re.search(words[k], line):
+                    occurrence[k] += 1
+print(occurrence)
