47d9094c1776c6de773f7a40a5f7caa337ad3392
LunatikTanuki <luna.hammesfahr@gmail.com>
2024-02-15 17:16:45 +0100

    Empty HF API token file

--- hf_token.py
+++ hf_token.py
@@ -3,4 +3,4 @@ Save your Hugging Face Hub API token instead of 'hf_xxx'.
 """
 import os
 
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = 'hf_xxx'
